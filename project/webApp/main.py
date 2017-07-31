6164ad67d68eda2c278a7ad7f20b6c1131c83b7c
Emmabyrne36 <emmabyrne36@gmail.com>
2017-07-31 18:03:57 +0100

    More style changes and overhaul of flask

--- project/webApp/main.py
+++ project/webApp/main.py
@@ -10,50 +10,62 @@ from webApp import app
 from flask import render_template, request, jsonify
 from webApp.Connect_DB import connect_db
 import json
+import time
+from datetime import datetime
 
 @app.route("/", methods=['GET', 'POST'])
 def index():
     return render_template('index.html')
-    #return render_template('indexCopy.html')
 
 
 @app.route('/test', methods=['POST'])
 def test():
     ''' Test function to get time predictions from db '''
-    engine = connect_db('team1010-test.cnmhll8wqxlt.us-west-2.rds.amazonaws.com', '3306', 'Team1010_Test', 'root', 'password.txt')
-    print("time is: ", request.form['time'])
-    print("date is: ", request.form['date'])
+    engine = connect_db('team1010.cnmhll8wqxlt.us-west-2.rds.amazonaws.com', '3306', 'Team1010', 'Team1010_User', 'password.txt')
     Origin=request.form['origin']
     Destination=request.form['destination']
+    Time=request.form['time']
+    # Idea to split date format dd/mm/yy into day, month and date adapted from https://stackoverflow.com/questions/4056683/python-getting-weekday-from-an-input-date
+    inputDate = time.strftime("%A %B %C", time.strptime(request.form['date'], "%d/%m/%Y"))
+    Day = inputDate.split()[0]
+    Month = inputDate.split()[1]
+    Date = inputDate.split()[2]
     # This is just a random query I made to test functionality of more complex queries, I set my table up only to have stopID from 1-10
-    sql = "SELECT ABS(t2.time - t1.time) FROM time_table t1, time_table t2 where t1.stopID = %s and t2.stopID = %s;"
-    rows = engine.execute(sql, Origin, Destination).fetchall() 
-    engine.dispose()
-    #return (origin=Origin, destination=Destination, time=int(rows[0][0]))
-    return render_template('form.html', origin=Origin, destination=Destination, time=int(rows[0][0]))
- 
-@app.route('/test2', methods=['GET', 'POST'])
-def test2():
-    ''' Test function to access data submitted via form and display on front end '''
-    engine = connect_db('team1010-test.cnmhll8wqxlt.us-west-2.rds.amazonaws.com', '3306', 'Team1010_Test', 'root', 'password.txt')
-    Route=request.form['route']
-    sql = "SELECT * FROM test where route = %s;" # %s is a placeholder for route in this case
-    rows = engine.execute(sql, Route).fetchall()
-    info = jsonify(info=[dict(row) for row in rows])    
-    engine.dispose()
-    return info
-    #return render_template('index.html', Route=request.form['route'], Time=request.form['time'], Origin=request.form['origin'], Destination=request.form['destination'])   
+    #sql = "SELECT ABS(t2.time - t1.time) FROM time_table t1, time_table t2 where t1.stopID = %s and t2.stopID = %s;"
+    #rows = engine.execute(sql, Origin, Destination).fetchall() 
+    #engine.dispose()
+    #time=int(rows[0][0])
+    calc = abs(int(Origin) - int(Destination))
+    return render_template('form.html', journeyTime=Time, origin=Origin, destination=Destination, time=calc, day=Day, month=Month, date=Date)
    
-   
-@app.route('/routes', methods=['GET'])
-def routes():
-    ''' This function gets the corresponding stops for the route when the route is typed into the input field '''
+@app.route('/direction', methods=['GET'])
+def direction():
+    ''' This function gets the corresponding stops for the direction when the route is typed into the input field '''
     x = request.args.get('route')
-    engine = connect_db('team1010-test.cnmhll8wqxlt.us-west-2.rds.amazonaws.com', '3306', 'Team1010_Test', 'root', 'password.txt')
+    engine = connect_db('team1010.cnmhll8wqxlt.us-west-2.rds.amazonaws.com', '3306', 'Team1010', 'Team1010_User', 'password.txt')
+    # Get the direction
+    sql = "SELECT direction from TestTable_direction where route = %s;"
+    rows = engine.execute(sql, x).fetchall()
+    selectDirection = []
+    # rows returns a 2D array
+    for i in rows:
+        for j in i:
+            selectDirection.append(j)
     
+    output = {}
+    output['selectDirection'] = selectDirection
+    return jsonify(output)
+    
+
+@app.route('/stops', methods=['GET'])
+def stops():
+    ''' This function gets the corresponding stops for the direction along the chosen route '''
+    x = request.args.get('direction')
+    engine = connect_db('team1010.cnmhll8wqxlt.us-west-2.rds.amazonaws.com', '3306', 'Team1010', 'Team1010_User', 'password.txt')
     # Get origin stops - dividing this section into origin and destination stops may not be necessary
     # as the stops for origin and destination will be the same for that route
-    sql = "SELECT origin_stop FROM test2 where Route = %s order by origin_stop asc;"
+    #sql = "SELECT origin FROM TestTable where route = %s order by origin asc;"
+    sql = "SELECT origin from TestTable where direction=%s order by origin asc;"
     rows = engine.execute(sql, x).fetchall()
     originStops = []
     # rows returns a 2D array
@@ -62,16 +74,15 @@ def routes():
             originStops.append(j)
             
     # Get destination stops        
-    sql2 = "SELECT origin_stop FROM test2 where Route = %s order by destination_stop asc;"
-    rows2 = engine.execute(sql2, x).fetchall()
-    destinationStops = []
-    for i in rows2:
-        for j in i:
-            destinationStops.append(j)
+    #sql2 = "SELECT origin FROM TestTable where route = %s order by destination asc;"
+    #rows2 = engine.execute(sql2, x).fetchall()
+    #destinationStops = []
+    #for i in rows2:
+    #    for j in i:
+    #        destinationStops.append(j)
             
     engine.dispose()    
     output = {}
     output['originStops'] = originStops
-    output['destinationStops'] = destinationStops
-    return jsonify(output)
-
+    #output['destinationStops'] = destinationStops
+    return jsonify(output)  
--- project/webApp/static/script.js
+++ project/webApp/static/script.js
@@ -1,66 +1,99 @@
 // Ideas for only passing arguments of input field to Flask obtained from https://stackoverflow.com/questions/7849221/ajax-delay-for-search-on-typing-in-form-field
 // Code for populating the dropdown menus for origin and destination stops adapted from https://stackoverflow.com/questions/41268096/getting-flask-to-alter-selected-value-in-html-drop-down
-
 var delayTimer;
-var route;
 
-function getRoute() {
-	console.log("Functon triggered, getting stops");
+function getDirection() {
+    // Function to get the directions that the route can go in - then populates the direction dropdown
+	console.log("Getting directions...");
 	clearTimeout(delayTimer);
 	delayTimer = setTimeout(function() {
 		var route = $('#route'),
-			origin = $('#origin'),
-			destination = $('#destination');
+			direction = $('#direction');
 		console.log("The route is " + route.val());
 
-		function updateOriginStops() {
-			// This will update the origin stop list corresponding to whatever route is typed in
-			updateSettings();
+		function updateDirection() {
+			// This will update the direction list corresponding to whatever route is typed in
+			updateDirectionSettings();
 		}
-
-		function updateDestinationStops() {
-			// This will update the destination stop list corresponding to whatever route is typed in - this may not be necessary in the end - origin and destination will have the same stops so may only need to have one function rather than 2
-			updateSettings();
-		}
-        
 		// Make the selections disabled while fetching new data
-		origin.attr('disabled', true);
-		destination.attr('disabled', true);
+		direction.attr('disabled', true);
 
-		function updateSettings() {
-			//route = document.getElementById("route").value;
-			var jqxhr = $.getJSON("http://127.0.0.1:5000/routes?route=" + route.val(), null, function(data) {
+		function updateDirectionSettings() {
+			var jqxhr = $.getJSON("http://127.0.0.1:5000/direction?route=" + route.val(), null, function(data) {
+				direction.empty();
                 
-				// Populate the origin stops
-				origin.empty();
-				$.each(data.originStops, function(originStops, value) {
-					origin.append($('<option>', {
+                // Populate the first option in direction with "Select direction" - this will be disabled and won't be able to be selected
+				direction.append($('<option>', {
+					value: "",
+					text: "Select direction"
+				}, '</option>'));
+				// To disable the first option in a dropdown menu - https://www.w3schools.com/jsref/prop_option_disabled.asp
+				document.getElementById("direction").options[0].disabled = true;
+                
+                // Populate direction with the correct values
+				$.each(data.selectDirection, function(selectDirection, value) {
+					direction.append($('<option>', {
 						value: value,
 						text: value
 					}, '</option>'));
 				});
-                
-				// Populate the destination stops
-				destination.empty();
-				$.each(data.destinationStops, function(destinationStops, value) {
-					destination.append($('<option>', {
-						value: value,
-						text: value
-					}, '</option>'));
-				});
-                
-				// Remove disabled selections after they have been populated
-				origin.removeAttr('disabled');
-				destination.removeAttr('disabled');
+				direction.removeAttr('disabled');
 			})
 		}
-        
-		// Call functions to update origin and destination stops
-		updateOriginStops();
-		updateDestinationStops();
-	}, 2000);
+		// Call function to update the direction dropdown menu
+		updateDirection();
+	}, 1500);
 }
 
+function getStops() {
+	console.log("Functon triggered, getting stops");
+	//clearTimeout(delayTimer);
+	//	delayTimer = setTimeout(function() {
+	var origin = $('#origin'),
+		destination = $('#destination'),
+		direction = $('#direction');
+	console.log("The direction is " + direction.val());
+
+	function updateStops() {
+		// This will update the origin stop list corresponding to whatever route is typed in
+		updateStopSettings();
+	}
+	//function updateDestinationStops() {
+	// This will update the destination stop list corresponding to whatever route is typed in - this may not be necessary in the end - origin and destination will have the same stops so may only need to have one function rather than 2
+	//	updateSettings();
+	//}
+	// Make the selections disabled while fetching new data
+	origin.attr('disabled', true);
+	destination.attr('disabled', true);
+
+	function updateStopSettings() {
+		//route = document.getElementById("route").value;
+		var jqxhr = $.getJSON("http://127.0.0.1:5000/stops?direction=" + direction.val(), null, function(data) {
+			// Populate the origin stops
+			origin.empty();
+			$.each(data.originStops, function(originStops, value) {
+				origin.append($('<option>', {
+					value: value,
+					text: value
+				}, '</option>'));
+			});
+			// Populate destination stops
+			destination.empty();
+			$.each(data.originStops, function(originStops, value) {
+				destination.append($('<option>', {
+					value: value,
+					text: value
+				}, '</option>'));
+			});
+			// Remove disabled selections after they have been populated
+			origin.removeAttr('disabled');
+			destination.removeAttr('disabled');
+		})
+	}
+	// Call functions to update origin and destination stops
+	updateStops();
+	//}, 500);
+}
 
 function getLiveWeather() {
 	//call weather API from openweathermap - https://openweathermap.org/current and https://openweathermap.org/weather-conditions
@@ -74,7 +107,6 @@ function getLiveWeather() {
 	})
 }
 
-
 // Ideas for using night mode adapted from - https://stackoverflow.com/questions/4358155/changing-background-based-on-time-of-day-using-javascript
 // This is a dynamic function which automatically sets the page to night mode if between 9pm and 6am
 function DayNight_Mode() {
@@ -86,33 +118,35 @@ function DayNight_Mode() {
 	}
 }
 
-// http://www.developphp.com/video/JavaScript/Change-Style-Sheet-Using-Tutorial-CSS-Swap-Stylesheet
+// Ideas for changing what CSS file is linked adapted from - http://www.developphp.com/video/JavaScript/Change-Style-Sheet-Using-Tutorial-CSS-Swap-Stylesheet
 function toggleNightMode() {
-    if (document.getElementById("toggle").checked) {
-        document.getElementById('pagestyle').setAttribute('href', '/static/nightMode.css');
-    } else {
-        document.getElementById('pagestyle').setAttribute('href', '/static/mainStyle.css');      
-    }
+	if (document.getElementById("toggle").checked) {
+		document.getElementById('pagestyle').setAttribute('href', '/static/nightMode.css');
+	} else {
+		document.getElementById('pagestyle').setAttribute('href', '/static/mainStyle.css');
+	}
 }
 
-               $(function () {
-                   $('#datePicker').datetimepicker({
-                       format: 'L',
-                       format: 'DD/MM/YYYY',
-                   });
-               });
 
-                $(function () {
-               $('.clockpicker').clockpicker({
-                   placement: 'bottom',
-                   align: 'left',
-                   autoclose: true,
-                   'default': 'now'
-               });
-                  });
-        
+// Code for datePicker adapted from
+// Code for clockpicker adapted from https://weareoutman.github.io/clockpicker/
+$(function() {
+	$('#datePicker').datetimepicker({
+		format: 'L',
+		format: 'DD/MM/YYYY',
+	});
+});
+$(function() {
+	$('.clockpicker').clockpicker({
+		placement: 'bottom',
+		align: 'left',
+		autoclose: true,
+		'default': 'now'
+	});
+});
 
-$( document ).ready(function() {
-    // Automatically trigger DayNight_Mode()
-    DayNight_Mode();
+
+$(document).ready(function() {
+	// Automatically trigger DayNight_Mode()
+	DayNight_Mode();
 });
